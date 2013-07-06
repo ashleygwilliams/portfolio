c98af60007a9243b96597d80284f74f88c5f08fb
ashleygwilliams <heyashleyashley@gmail.com>
2013-07-06 15:04:49 -0400

    initial commit

--- Gemfile
+++ Gemfile
@@ -1,10 +1,15 @@
-source :rubygems
+source "https://rubygems.org"
 
 gem "sinatra"
 gem "sqlite3"
 gem "activerecord"
 gem "sinatra-activerecord"
 gem "rake"
+
+gem "haml"
+gem "redcarpet"
+gem "sass"
+gem "compass"
 
 group :development do
   gem "shotgun"
--- app.rb
+++ app.rb
@@ -5,30 +5,59 @@ Dir.glob('./lib/*.rb') do |model|
   require model
 end
 
-module Name
+module Portfolio
   class App < Sinatra::Application
 
     #configure
     configure do
       set :root, File.dirname(__FILE__)
       set :public_folder, 'public'
+
+      Compass.configuration do |config|
+        config.project_path = File.dirname(__FILE__)
+        config.sass_dir = 'views'
+      end
+
+      set :haml, { :format => :html5 }
+      set :sass, Compass.sass_engine_options
+      set :scss, Compass.sass_engine_options
     end
 
-    #database
-    set :database, "sqlite3:///database.db"
-
     #filters
+    before do
+      @projects = YAML::load(File.open('db/projects.yml'))
+      @events = YAML::load(File.open('db/events.yml'))
+    end
 
     #routes
     get '/' do
-      erb :index
+      haml :index
+    end
+
+    get "/custom.css" do
+      scss :styles
     end
 
     #helpers
     helpers do
-      def partial(file_name)
-        erb file_name, layout => false
-      end
+      def partial(template,locals=nil)
+        if template.is_a?(String) || template.is_a?(Symbol)
+          template=('_' + template.to_s).to_sym
+        else
+          locals=template
+          template=template.is_a?(Array) ? ('_' + template.first.class.to_s.downcase).to_sym : ('_' + template.class.to_s.downcase).to_sym
+        end
+        if locals.is_a?(Hash)
+          haml(template,{:layout => false},locals)      
+        elsif locals
+          locals=[locals] unless locals.respond_to?(:inject)
+          locals.inject([]) do |output,element|
+            output <<     haml(template,{:layout=>false},{template.to_s.delete("_").to_sym => element})
+          end.join("\n")
+        else 
+          haml(template,{:layout => false})
+        end
+    end
     end
 
   end
